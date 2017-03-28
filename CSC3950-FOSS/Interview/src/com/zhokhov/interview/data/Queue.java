9b11f421407c1655d76ae208c7c00e6aad087348
BinaryBlacksmith <jring1911@live.com>
2017-03-28 10:23:00 -0700

    Updated Queue
    -Added user input.
    -Removed old,unnecessary functions
    -Interfaces with main

--- CSC3950-FOSS/Interview/src/com/zhokhov/interview/data/Queue.java
+++ CSC3950-FOSS/Interview/src/com/zhokhov/interview/data/Queue.java
@@ -1,77 +1,43 @@
 /*
- *  You are free to use this code anywhere, copy, modify and redistribute at your
- *  own risk.
- *  Your are solely responsibly for any damage that may occur by using this code.
- *
+ *  You are free to use this code anywhere, copy, modify and redistribute at your own risk.
+ *  Your are solely responsibly for any damage that may occur by using this code
  *  This code is not tested for all boundary conditions. Use it at your own risk.
- *
  *  Original code: http://www.mycstutorials.com/articles/data_structures/queues
+ *  @author <a href='mailto:alexey@zhokhov.com'>Alexey Zhokhov</a>
  */
+ //Ring 3/28/17: Removed several functions made unnecesary by QueueStart.
 package com.zhokhov.interview.data;
-
 import java.util.Scanner;
 
-/**
- * @author <a href='mailto:alexey@zhokhov.com'>Alexey Zhokhov</a>
- */
 public class Queue {
+   private LinkedList list;
 
-    private LinkedList list;
+   public boolean isEmpty(){// Post: Returns true if the queue is empty. Otherwise, false.
+      return (list.size() == 0);
+   }
 
-    // Queue constructor
-    public Queue() {
-        // Create a new LinkedList.
-        list = new LinkedList();
-    }
-
-    public boolean isEmpty()
-    // Post: Returns true if the queue is empty. Otherwise, false.
-    {
-        return (list.size() == 0);
-    }
-
-    public void enqueue(Object item)
-    // Post: An item is added to the back of the queue.
-    {
-        // Append the item to the end of our linked list.
-        list.add(item);
-    }
-
-    public Object dequeue()
-    // Pre: this.isEmpty() == false
-    // Post: The item at the front of the queue is returned and
-    //         deleted from the queue. Returns null if precondition
-    //         not met.
-    {
-        // Store a reference to the item at the front of the queue
-        //   so that it does not get garbage collected when we
-        //   remove it from the list.
-        // Note: list.get(...) returns null if item not found at
-        //   specified index. See postcondition.
-        Object item = list.get(1);
-        // Remove the item from the list.
-        // My implementation of the linked list is based on the
-        //   J2SE API reference. In both, elements start at 1,
-        //   unlike arrays which start at 0.
-        list.remove(1);
-
-        // Return the item
-        return item;
-    }
-
-    public Object peek()
-    // Pre: this.isEmpty() == false
-    // Post: The item at the front of the queue is returned and
-    //         deleted from the queue. Returns null if precondition
-    //         not met.
-    {
-        // This method is very similar to dequeue().
-        // See Queue.dequeue() for comments.
-        return list.get(1);
-    }
-
-    public void QueueStart(Scanner scanQ){
-    	
-    }
-    
+   public void QueueStart(Scanner scanQ){
+      int Qinput=0;
+      list = new LinkedList();// My implementation of the linked list is based on the J2SE API reference. In both, elements start at 1, unlike arrays which start at 0. 
+      while(Qinput!=-999){
+         System.out.println("Enter the values you would like to add to the queue.(-999 to stop)");
+         Qinput=scanQ.nextInt();
+         if(Qinput!=-999){
+            list.add(Qinput);
+         }
+      }
+      Qinput=0;
+      while(Qinput!=-999){
+         System.out.println("What position would you like to see the value for?(-999 to stop)");    
+         Qinput=scanQ.nextInt();
+         System.out.println(list.get(Qinput));
+      }
+      Qinput=0;
+      while(Qinput!=-999){
+         System.out.println("What value would you like to remove?(-999 to stop)");    
+         Qinput=scanQ.nextInt();
+         list.remove(Qinput);
+         System.out.println("Removed: "+Qinput);
+      }
+   }
 }
